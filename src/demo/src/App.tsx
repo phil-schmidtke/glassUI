2f3a4e818a27d5e115c520ca3e65b2a7d3d8046f
phil-schmidtke <mail@phil-schmidtke.de>
2022-12-20 15:46:28 +0100

    demo for buttons and card

--- src/demo/src/App.tsx
+++ src/demo/src/App.tsx
@@ -1,8 +1,27 @@
+
 function App() {
 
   return (
-    <div className="container-sm">
-      <p className='btn-outline'>test</p>
+    <div className="m-4">
+      <div>
+        <h1 className="text-center font-bold text-2xl mt-4">GlassUI - A TailwindCss Component Libary</h1>
+      </div>
+      <div>
+        <h2>Buttons</h2>
+        <div className="mb-2">
+          <button className="btn">Standart Button</button>
+        </div>
+        <div className="my-2">
+          <button className="btn-outline">Outline Button</button>
+        </div>
+        <div className="my-2">
+          <button className="btn-sm">Small Button</button>
+        </div>
+        <div className="my-2">
+          <button className="btn-outline-sm">Small Button</button>
+        </div>
+      </div>
+      <div className="card">test</div>
     </div>
   )
 }
