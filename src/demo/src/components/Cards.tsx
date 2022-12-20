0f310cb5a55ede2f17770834244050a61e9f34f9
phil-schmidtke <mail@phil-schmidtke.de>
2022-12-20 21:24:48 +0100

    responsive fix

--- src/demo/src/components/Cards.tsx
+++ src/demo/src/components/Cards.tsx
@@ -1,15 +1,17 @@
 export default function Cards() {
     return (
-        <div className="mt-4 md:flex">
+        <div>
             <h2 className="ml-2">Cards</h2>
-            <div className="w-full md:mx-2">
-                <div className="card">Card</div>
-            </div>
-            <div className="w-full md:mx-2 mt-4 md:mt-0">
-                <div className="card-light">Card Light</div>
-            </div>
-            <div className="w-full md:mx-2 mt-4 md:mt-0">
-                <div className="card-outline">Outline Card</div>
+            <div className="mt-4 md:flex">
+                <div className="w-full md:mx-2">
+                    <div className="card">Card</div>
+                </div>
+                <div className="w-full md:mx-2 mt-4 md:mt-0">
+                    <div className="card-light">Card Light</div>
+                </div>
+                <div className="w-full md:mx-2 mt-4 md:mt-0">
+                    <div className="card-outline">Outline Card</div>
+                </div>
             </div>
         </div>
     )
