1ab8617bbb790f50eb179b7cceec59c744143077
phil-schmidtke <mail@phil-schmidtke.de>
2022-12-20 21:31:46 +0100

    input fixes

--- src/demo/src/components/Inputs.tsx
+++ src/demo/src/components/Inputs.tsx
@@ -3,13 +3,13 @@ export default function Inputs() {
         <div className="mt-4">
             <h1 className="ml-2">Inputs</h1>
             <div className="">
-                <div className="flex">
-                    <input className="input block" placeholder="Input" />
-                    <input className="input-sm ml-2" placeholder="Medium Input" />
+                <div className="block sm:flex">
+                    <input className="input block w-full" placeholder="Input" />
+                    <input className="input-sm mt-2 sm:mt-0 sm:ml-2 w-full" placeholder="Medium Input" />
                 </div>
-                <div className="flex mt-2">
-                    <input className="input-outline" placeholder="Outline Input" />
-                    <input className="input-outline-sm ml-2" placeholder="Medium Outline Input" />
+                <div className="block sm:flex mt-2">
+                    <input className="input-outline block w-full" placeholder="Outline Input" />
+                    <input className="input-outline-sm mt-2 sm:mt-0 sm:ml-2 w-full" placeholder="Medium Outline Input" />
                 </div>
             </div>
         </div>
