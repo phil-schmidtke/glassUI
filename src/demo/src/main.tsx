849463fd4f7f7611537cc687f52b8a3c1e3473f9
phil-schmidtke <mail@phil-schmidtke.de>
2022-12-20 22:38:01 +0100

    particles

--- src/demo/src/main.tsx
+++ src/demo/src/main.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import HeaderContent from './components/HeaderContent'
+import ParticleBackground from 'react-particle-backgrounds'
 import './index.css'
+
+const settings = {
+  particle: {
+    particleCount: 75,
+    color: "#fff",
+    minSize: 1,
+    maxSize: 8
+  },
+  velocity: {
+    minSpeed: 0.2,
+    maxSpeed: 0.4
+  },
+  opacity: {
+    minOpacity: 0,
+    maxOpacity: 0.6,
+    opacityTransitionTime: 10000
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <div className='absolute w-screen h-screen'>
+      <ParticleBackground settings={settings} />
+    </div>
     <App />
   </React.StrictMode>,
 )
