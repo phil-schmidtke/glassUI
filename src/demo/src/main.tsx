cb9c0993c2e85b8a4d8c51de633fc18f90448692
phil-schmidtke <mail@phil-schmidtke.de>
2022-12-20 22:44:56 +0100

    change versions again

--- src/demo/src/main.tsx
+++ src/demo/src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 //@ts-ignore
 import ParticleBackground from 'react-particle-backgrounds'
