bfa7212dacd4c0cf67fc50274f21f52614407eb1
phil.schmidtke <phil.schmidtke@telekom.de>
2022-12-20 10:28:58 +0100

    setup

--- index.js
+++ index.js
@@ -1,60 +1,18 @@
-module.exports = {
-    theme: {
-        extend: {
-        backgroundColor: {
-            'red-500': '#ff0000',
-        },
-        },
-        button: {
-        // Define your custom button styles here
-        'red': '#ff0000',
-        'blue': '#0000ff',
-        'green': '#00ff00',
-        'gray': '#cccccc',
-        },
-    },
-    variants: {
-        // Add your custom variants here
-    },
-    plugins: [
-        function({ addComponents, theme }) {
-        const buttons = {
-            '.btn': {
-            display: 'inline-block',
-            fontWeight: 'bold',
-            textAlign: 'center',
-            whiteSpace: 'nowrap',
-            verticalAlign: 'middle',
-            userSelect: 'none',
-            border: '1px solid transparent',
-            padding: '.375rem .75rem',
-            fontSize: '1rem',
-            lineHeight: '1.5',
-            borderRadius: '.25rem',
-            transition: 'color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out',
-            '&:disabled': {
-                opacity: '.65',
-            },
-            },
-            '.btn-red': {
-            backgroundColor: theme('button.red'),
-            color: '#ffffff',
-            },
-            '.btn-blue': {
-            backgroundColor: theme('button.blue'),
-            color: '#ffffff',
-            },
-            '.btn-green': {
-            backgroundColor: theme('button.green'),
-            color: '#ffffff',
-            },
-            '.btn-gray': {
-            backgroundColor: theme('button.gray'),
-            color: '#ffffff',
-            },
-        };
+// tailwind css compoenent plugin
+const plugin = require('tailwindcss/plugin')
 
-        addComponents(buttons);
+module.exports = plugin(function ({ addComponents, theme }) {
+    const components = {
+        '.btn': {
+            padding: theme('spacing.2'),
+            borderRadius: theme('borderRadius.lg'),
+            backgroundColor: theme('colors.blue.500'),
+            color: theme('colors.white'),
+            '&:hover': {
+                backgroundColor: theme('colors.blue.600'),
+            },
         },
-    ],
-}
+    }
+    addComponents(components)
+    }
+)
---
+++ src/demo/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="container-sm">
+      <p className='container p-4 border-indigo-500 border-2'>test</p>
+    </div>
+  )
+}
+
+export default App
