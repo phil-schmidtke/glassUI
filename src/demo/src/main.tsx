bb9c36deb5d2458c336e185ab66cf2de87697374
phil-schmidtke <mail@phil-schmidtke.de>
2022-12-20 23:03:48 +0100

    new particles

--- src/demo/src/main.tsx
+++ src/demo/src/main.tsx
@@ -1,32 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-//@ts-ignore
-import {ParticleBackground} from 'react-particle-backgrounds'
+import ParticlesBg from 'particles-bg'
 import './index.css'
 
-const settings = {
-  particle: {
-    particleCount: 75,
-    color: "#fff",
-    minSize: 1,
-    maxSize: 8
-  },
-  velocity: {
-    minSpeed: 0.2,
-    maxSpeed: 0.4
-  },
-  opacity: {
-    minOpacity: 0,
-    maxOpacity: 0.6,
-    opacityTransitionTime: 10000
-  }
-}
+let config = {
+  num: [1, 3],
+  rps: 0.2,
+  radius: [3, 20],
+  life: [1.5, 10],
+  v: [0.01, 0.03],
+  tha: [-10, 10],
+  alpha: [0.4, 0],
+  scale: [.1, 0.9],
+  position: "all",
+  color: ["random", "#FFFFFF"],
+  cross: "dead",
+  random: 8,
+  g: 0.2,
+};
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className='absolute w-screen h-screen'>
-      <ParticleBackground settings={settings} />
+    <ParticlesBg type="custom" bg={false} config={config} />
     </div>
     <App />
   </React.StrictMode>,
