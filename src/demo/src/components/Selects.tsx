4327e5e07ef76274e6032c8b3245490e69bfae90
phil-schmidtke <mail@phil-schmidtke.de>
2022-12-22 17:59:58 +0100

    class name fix

--- src/demo/src/components/Selects.tsx
+++ src/demo/src/components/Selects.tsx
@@ -58,7 +58,7 @@ export default function Selects() {
                             </span>
                             {selected ? (
                                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                     </svg>
                                 </span>
